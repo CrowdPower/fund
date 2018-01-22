0a382d367e0ccb3351168db88af6042a3087fa26
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-01-22 00:44:12 -0500

    Add config file

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/crowdpower/fund/server"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	getConfig()
+
+	port := viper.GetString("server.port")
+	cert := viper.GetString("server.cert")
+	key := viper.GetString("server.key")
 	r := server.Router()
-	port := ":8080"
-	cert := "server.crt"
-	key := "server.key"
-	log.Printf("Starting server on %v", port)
-	log.Fatal(http.ListenAndServeTLS(port, cert, key, r))
+
+	log.Printf("Listening on port %v", port)
+	log.Fatal(http.ListenAndServeTLS(":"+port, cert, key, r))
+}
+
+func getConfig() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetDefault("server", map[string]string{
+		"port": "8080",
+		"cert": "server.crt",
+		"key":  "server.key",
+	})
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %v \n", err))
+	}
 }
